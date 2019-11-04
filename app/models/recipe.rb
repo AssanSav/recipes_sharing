bd042cbc8cdb02eab95c7de3301e5c67c6bf86e0
Assane Savadogo <assane.savadogo81@gmail.com>
2019-11-04 01:40:03 -0500

    Associations and validation in the models classes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,8 @@
 class Recipe < ActiveRecord::Base
+    belongs_to :user
+    has_many :comments
+
+    validates :name, presence: true
+    validates :ingredient, presence: true 
+    validates :process, presence: true
 end
---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+Recipe.create(name: "Fresh Almond and Pomegranate Salad", ingredient: "1 cup raw organic almonds
+1-2 cups pomegranate seeds (depending on your ratio preference)
+Extra Virgin Olive Oil to coat (worth it to splurge for the good stuff here
+Salt to taste
+Optional: a chopped fresh herb such as mint or parsley as preferred.", process: "In a medium mixing bowl, add all ingredients and stir.
+Adjust olive oil and salt to taste.
+Serve immediately.")
