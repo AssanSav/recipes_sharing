e39783962fdccfc1f49ba6f0a25c07726221e321
Assane Savadogo <assane.savadogo81@gmail.com>
2019-11-04 01:49:57 -0500

    Users signup GET & POST routes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,18 @@
+class UsersController < ApplicationController
+
+  get "/signup" do
+    erb :"/users/signup.html"
+  end
+
+  post "/signup" do
+    @user = User.create(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password], password_confirmation: params[:password_confirmation])
+    if params[:user][:username] !=nil && params[:user][:email] !=nil && params[:user][:password] !=nil && params[:password_confirmation] !=nil
+      session[:user_id] = @user.id
+      redirect to '/recipes/new'
+    else
+      flash[:message] = "Check Your Inputs! No Column Can't Be left Blank!"
+    erb :"/users/signup.html"
+    end
+  end
+
+end
