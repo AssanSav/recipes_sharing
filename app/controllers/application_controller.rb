b81b8252b1e1fad6490d580b7cf18178d4b39e86
Assane Savadogo <assane.savadogo81@gmail.com>
2019-11-04 03:05:14 -0500

    Adding helpers in application_controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,32 @@
 require './config/environment'
+require 'rack-flash'
 
 class ApplicationController < Sinatra::Base
 
-  configure do
-    set :public_folder, 'public'
-    set :views, 'app/views'
-  end
+    configure do
+        set :public_folder, 'public'
+        set :views, 'app/views'
+    end
 
-  get "/" do
-    erb :index
-  end
+    get "/" do
+        erb :index
+    end
 
-  
+    helpers do 
+
+        def current_user 
+            @current_user ||= User.find_by_id(session[:user_id])
+        end
+
+        def logged_in?
+            !current_user.nil?
+        end
+
+        def redirect_if_not_logged_in?
+            if !logged_in? 
+                redirect to '/'
+            end
+        end
+    end
 
 end
--- app/controllers/seession_controllers.rb
+++ app/controllers/seession_controllers.rb
@@ -7,11 +7,11 @@ class SessionController < ApplicationController
     post "/login" do
         @user = User.find_by_username(params[:user][:username])
         if @user && @user.authenticate(params[:user][:password])
-          session[:user_id] = @user.id 
-          redirect to "/recipes/new"
+            session[:user_id] = @user.id 
+            redirect to "/recipes/new"
         else 
-          flash[:message] = "Invalid Username Or Password!"
-        erb :"/users/login.html"
+            flash[:message] = "Invalid Username Or Password!"
+            erb :"/users/login.html"
         end
     end
     
