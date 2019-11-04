b27f9e882692d8b07084910b829a134c38c66700
Assane Savadogo <assane.savadogo81@gmail.com>
2019-11-04 02:13:25 -0500

    Recipes POST/create route

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -11,5 +11,17 @@ class RecipesController < ApplicationController
         erb :"/recipes/new.html"
     end
 
+    post "/recipes" do
+        @recipe = current_user.recipes.new(name: params[:recipe][:name], ingredient: params[:recipe][:ingredient], process: params[:recipe][:process])
+        if !params[:recipe][:name].empty? && !params[:recipe][:ingredient].empty? && !params[:recipe][:process].empty?
+            @recipe.save
+            @recipe
+            flash[:message] = "You successfully Created A New Recipe"
+            redirect ("/recipes/#{@recipe.slug}")
+        else
+            flash[:message] = "No Column Can't Be Left Blank!!!"
+            redirect to "/recipes/new"
+        end
+    end
     
 end
