86eb8aca45f7c7f7d51653d355e90a4be11699a9
Assane Savadogo <assane.savadogo81@gmail.com>
2019-11-05 12:20:17 -0500

    DRY comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,14 +3,13 @@ class CommentsController < ApplicationController
     post "/recipes/:recipe_id/comments" do
         @recipe = Recipe.find_by_id(params[:recipe_id])
         if @recipe
-            @comment = current_user.comments.create(content: params[:comment][:content] )
+            @comment = current_user.comments.new(content: params[:comment][:content] )
             @comment.recipe_id = @recipe.id
             @comment.save
             flash[:message] = "Comment Can't Be Blank!"
             redirect "/recipes/#{@recipe.id}"
         end
     end
-    
 
     get "/recipes/:recipe_id/comments/:id/edit" do 
         @recipe = Recipe.find_by_id(params[:recipe_id])
@@ -24,7 +23,6 @@ class CommentsController < ApplicationController
         end
     end
     
-    
     patch "/recipes/:recipe_id/comments/:id" do
         @recipe = Recipe.find_by_id(params[:recipe_id])
         @comment = Comment.find_by_id(params[:id])
@@ -33,7 +31,6 @@ class CommentsController < ApplicationController
             @comment.recipe_id = @recipe.id
             @comment.update(content: params[:comment][:content])
             redirect to "/recipes/#{@recipe.id}"
-            binding.pry
         else
             erb :"/comments/edit.html"
         end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ActiveRecord::Base
     belongs_to :user
-    has_many :comments#, through: :users
+    has_many :comments
 
     validates :name, presence: true
     validates :ingredient, presence: true 
@@ -9,18 +9,5 @@ class Recipe < ActiveRecord::Base
     def self.visible
         where(deleted: false)
     end
-
-    def slug 
-        @name = name.split(" ") 
-        @slug = @name.join("-").downcase
-    end
-
-    def self.find_by_slug(slug)
-        Recipe.all.each do |recipe| 
-            if recipe.slug == slug 
-               return recipe
-            end
-        end
-        nil
-    end
+    
 end
